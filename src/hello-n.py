c8ff4cadb2a1d4a237bed96c53c9a9922c05bc5d
AnastasiaKratschmer <111846502+AnastasiaKratschmer@users.noreply.github.com>
2022-09-05 17:05:01 +0200

    med ekstra linje

--- src/hello-n.py
+++ src/hello-n.py
@@ -1,6 +1,9 @@
 
 # this asks the user for a number and turns the answer
 # into an integer
+from inspect import EndOfBlock
+
+
 n = int(input("How many times should I loop? "))
 # Now, print "Hello, World!" n times.
 i=0
