706cd502df910b69714853663e296d17597809dc
AnastasiaKratschmer <111846502+AnastasiaKratschmer@users.noreply.github.com>
2022-09-08 12:56:29 +0200

    looper ikke uendeligt

--- src/hello-n.py
+++ src/hello-n.py
@@ -6,7 +6,7 @@ from inspect import EndOfBlock
 
 n = int(input("How many times should I loop? "))
 # Now, print "Hello, World!" n times.
-i=0
+i=1
 while i <= n:
     print ("Hello, World!")
-    i+=1
+    i+=1 # i = i + 1
